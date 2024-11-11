d4283569de885caa5aba97dea0ff753700f81dbb
Kondareddy Ambavaram <118420084+Kondareddy1209@users.noreply.github.com>
2024-11-11 20:26:32 +0530

    guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -1,32 +1,31 @@
+from flask import Flask, render_template, request, redirect, url_for
 import random
+app = Flask(__name__)
+jackpot = random.randint(1, 100)
+counter = 0
+@app.route('/', methods=['GET', 'POST'])
+def guess_game():
+    global counter, jackpot
 
-jackpot=random.randint(1,100)
+    if request.method == 'POST':
+        try:
+            number = int(request.form['number'])
+        except ValueError:
+            return render_template('index.html', message="Please enter a valid number", attempts=counter)
+        
+        counter += 1
 
-counter=1
+        if number < jackpot:
+            return render_template('index.html', message="Wrong Guess! Please Guess Higher", attempts=counter)
+        elif number > jackpot:
+            return render_template('index.html', message="Wrong Guess! Please Guess Lower", attempts=counter)
+        else:
+            result_message = f"Correct Guess! Your attempts: {counter}"
+            jackpot = random.randint(1, 100)
+            counter = 0
+            return render_template('index.html', message=result_message, attempts=0)
 
-print("Welcome to the Number Guessing Game!")
+    return render_template('index.html', message="", attempts=counter)
 
-print("Try to guess the number between 1 and 100.")
-
-number=int(input("Enter the input From the user: "))
-
-while number!=jackpot:
-
-    if number<jackpot:
-
-        print("Wrong Guess! please Guess Higher")
-    else:
-
-        print("Wrong Guess! please Guess Lower")
-
-    number=int(input("Enter the input: "))
-    
-    counter+=1
-
-else:
-    print("Correct Guess")
-
-    print("Your Guess are : ")
-
-    print("Your attempt is: ",counter)
-
+if __name__ == '__main__':
+    app.run(debug=True)
