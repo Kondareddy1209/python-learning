69494671a8229f2303679ec7e626be47628b5951
Ambavaram Tirumala Kondareddy <118420084+Kondareddy1209@users.noreply.github.com>
2025-07-26 09:46:23 +0530

    Create sum_of_elements.py

---
+++ sum_of_elements.py
@@ -0,0 +1,12 @@
+# Define a list of numbers (with some duplicates)
+Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 100, 2, 3]
+
+# Initialize a variable to store the sum
+sum = 0
+
+# Loop through each element in the list and add it to sum
+for i in Arr:
+    sum += i
+
+# Print the total sum of elements
+print(sum)
