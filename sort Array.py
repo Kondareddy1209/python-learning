42cc9cecca32b291fe90e0e4d32a408b19d565f4
Ambavaram Tirumala Kondareddy <118420084+Kondareddy1209@users.noreply.github.com>
2025-07-26 14:55:06 +0530

    Create sort Array.py

---
+++ sort Array.py
@@ -0,0 +1,6 @@
+# Define a list of integers with multiple zeros and other numbers
+new = [0, 1, 0, 2, 4, 0, 0, 6, 0, 2, 3, 4, 5, 0, 1, 6, 0, 50]
+
+# Print the sorted version of the list in ascending order
+# This does not modify the original 'new' list, it returns a new sorted list
+print(sorted(new))
