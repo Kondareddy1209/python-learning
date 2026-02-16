f73792d1f3ea4446bb9c0016aa66a68675e3d5d5
Kondareddy <rkonda959@gmail.com>
2026-02-16 14:27:59 +0530

    js files included

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,76 @@
+// const profile={
+//     username: "shradhakhapar",
+//     isfollow:true,
+//     message: "click here for message ",
+//     posts: 150 ,
+//     followers: 150,
+//     description : "Hi i'm an entraprenuer"
+// }
+// console.log(profile)
+
+
+// const student={
+//     fullname : "Ambavaram Tirumala Kondareddy",
+//     phone : 9392887268 , 
+//     gender : "Male" , 
+//     Address : "Akkalareddypalli "
+// }
+// console.log(student)
+
+// let a = 5;
+// let b = 10;
+// c= a+b 
+// d=a-b
+// e=a*b
+// f=a/b
+// console.log("a+b = ",c,d,e,f)
+
+
+//conditinal Statements
+
+// let age=18;
+// if (age >18){
+//     age='he is eligible for vote'
+// }else{
+//     age='he is not eligible to vote'
+// }
+
+// console.log(age)
+
+
+// let x=18
+// x=x+1
+// if (x%2===0){
+//     x='it is even'
+// }else{
+//     x='it is not even'
+// }
+// console.log(x)
+
+// let vote=18;
+// if (vote<18){
+//     vote='he is not eligible to vote'
+// }else if(vote>18){
+//     vote='he is eligible to vote'
+// }else{
+//     vote='he do not have vote to cast'
+// }
+// console.log(vote)
+
+// let x=1 ;
+// x>18 ? console.log("u can vote ") : console.log( "u cannt vote")
+
+//Switch Case
+let varac="laddu"
+switch(varac){
+    case 'mangoes':
+        console.log("Fruits are not matched");
+        break;
+    case 'Apples':
+        console.log("Fruits are matched")
+        break
+    case 'Papayas':
+        console.log("papayas are there");
+    default:
+        console.log("No Fruit is matched");
+}
